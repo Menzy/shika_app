38715356eb1f74e26b8eddfa424d3278b3fca65e
Menzy <joshuamensah46@gmail.com>
2024-11-03 20:11:53 +0000

    Add CurrencyTransaction model for handling currency transactions

---
+++ lib/models/currency_transaction.dart
@@ -0,0 +1,29 @@
+class CurrencyTransaction {
+  final String currencyCode;
+  final double amount;
+  final DateTime timestamp;
+  final String type;  // 'addition', 'subtraction', etc.
+
+  CurrencyTransaction({
+    required this.currencyCode,
+    required this.amount,
+    required this.timestamp,
+    required this.type,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'currencyCode': currencyCode,
+    'amount': amount,
+    'timestamp': timestamp.toIso8601String(),
+    'type': type,
+  };
+
+  factory CurrencyTransaction.fromJson(Map<String, dynamic> json) {
+    return CurrencyTransaction(
+      currencyCode: json['currencyCode'],
+      amount: json['amount'],
+      timestamp: DateTime.parse(json['timestamp']),
+      type: json['type'],
+    );
+  }
+}
