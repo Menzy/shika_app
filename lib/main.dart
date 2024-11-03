8f49c29e7d68aa619261db363e8e38885eb58f57
Menzy <joshuamensah46@gmail.com>
2024-11-03 20:10:53 +0000

    Initialize Firebase and update app structure: add AuthProvider and replace NavigationMenu with SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kukuo/providers/auth_provider.dart';
+import 'package:kukuo/splash_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:kukuo/navigation_menu.dart';
 import 'providers/exchange_rate_provider.dart';
 import 'providers/user_input_provider.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
-  // debugPaintSizeEnabled = true;
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -23,12 +30,13 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => ExchangeRateProvider()),
         ChangeNotifierProvider(create: (_) => UserInputProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'Euclid'),
-        title: 'Currency Converter',
-        home: const NavigationMenu(),
+        title: 'Pot of Gold',
+        home: const SplashScreen(),
       ),
     );
   }
